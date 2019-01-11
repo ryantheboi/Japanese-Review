334d26601bdfa067d488b27cddc4db2e0402f4ec
Ryan Chung <ryan.chung851@gmail.com>
2019-01-11 02:54:24 -0500

    initial commit

---
+++ Japanese Review/JapaneseWord.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Japanese_Review
+{
+    public class JapaneseWord
+    {
+        private String[] english;
+        private String[] japanese;
+        private String romanization;
+        private String writing;
+        private int kanji;
+        private int chapter;
+    }
+}
---
+++ Japanese Review/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Japanese_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
