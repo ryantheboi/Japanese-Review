80592580d3875cd3423db0baffec5174b50242f7
Ryan Chung <ryan.chung851@gmail.com>
2019-03-16 19:15:11 -0400

    added string parsing to specify which chapters of Genki to initialize

--- Japanese Review/Program.cs
+++ Japanese Review/Program.cs
@@ -5,18 +5,86 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Japanese_Review
 {
     class Program
     {
+        /*
+         * 
+         */
+        
+        //Sample 02: Define Task/Wait Callback function
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("");
             // 20 chapters of vocab from the genki textbook
+            
+            Console.WriteLine("What chapter(s) would you like to be quizzed on?");
+            Console.WriteLine("Option examples: [15] [1, 15, 20] [1-20] [1-5, 15-20] (exclude brackets)");
+            var chapterString = Console.ReadLine();
+            
+            // convert the input string into an array of int (chapters) to pass to initGenki()
+            var chapters = new int[20]; // there can be no more than 20 chapters of vocab from genki
+            var idx = 0;
+            
+            // if there are multiple chapters separated by ','
+            if (chapterString.Contains('-'))
+            {
+                var chapterArray = chapterString.Split(',');
+                foreach (var c in chapterArray)
+                {
+                    // strip the whitespace from the string
+                    var chapter = c.Replace(" ", "");
+                    
+                    // add multiple chapters indicated by '-'
+                    if (chapter.Contains('-'))
+                    {
+                        var startEnd = chapter.Split('-');
+                        var start = Convert.ToInt32(startEnd[0]);
+                        var end = Convert.ToInt32(startEnd[1]);
+                        for (var i = start; i <= end; i++)
+                        {
+                            chapters[idx] = i;
+                            idx++;
+                        }
+                    }
+                    else // just a chapter number
+                    {
+                        chapters[idx] = Convert.ToInt32(chapter);
+                        idx++;
+                    }
+                }
+            }
+            
+            // else if there are multiple chapters indicated by '-'
+            else if (chapterString.Contains('-'))
+            {
+                var startEnd = chapterString.Split('-');
+                var start = Convert.ToInt32(startEnd[0]);
+                var end = Convert.ToInt32(startEnd[1]);
+                for (var i = start; i <= end; i++)
+                {
+                    chapters[idx] = i;
+                    idx++;
+                }
+            }
+
+            else // just a single chapter number
+            {
+                chapters[idx] = Convert.ToInt32(chapterString);
+            }
+            
             var genkiVocab = new JapaneseWord[20][];
             genkiVocab = JapaneseWord.initGenki();
+            
+
         }
     }
 }
